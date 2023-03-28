71bed05f4b126abc28fbed45030f6785babbf37d
patienceigiraneza <patienceigiraneza2@gmail.com>
2023-03-28 17:09:48 +0200

    Create full database and add in SQLITE

--- apiapp/admin.py
+++ apiapp/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+
+admin.site.register(CustomUser)
+admin.site.register(Client)
+admin.site.register(Supporter)
+admin.site.register(Board)
+admin.site.register(Conversation)
+admin.site.register(Message)
+admin.site.register(Category)
--- apiapp/models.py
+++ apiapp/models.py
@@ -25,10 +25,10 @@ class CustomUser(AbstractUser):
 
 
 
-@receiver(post_save, sender=settings.AUTH_USER_MODEL)
-def create_auth_token(sender, instance=None, created=False, **kwargs):
-    if created:
-        Token.objects.create(user=instance)
+# @receiver(post_save, sender=settings.AUTH_USER_MODEL)
+# def create_auth_token(sender, instance=None, created=False, **kwargs):
+#     if created:
+#         Token.objects.create(user=instance)
 
 
 class Client(models.Model):
