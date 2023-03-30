10750afd4d741350fb0d387067c2b0c291982e92
patienceigiraneza <patienceigiraneza2@gmail.com>
2023-03-30 09:24:16 +0200

    Add formatting

--- apiapp/serializers.py
+++ apiapp/serializers.py
@@ -6,32 +6,38 @@ class CustomerUserSerializer(serializers.ModelSerializer):
         model = CustomUser
         fields = '__all__'
 
+
 class ClientSerializer(serializers.ModelSerializer):
     class Meta:
         model = Client
         fields = '__all__'
-        
+
+
 class CategorySerializer(serializers.ModelSerializer):
     class Meta:
         model = Category
         fields = '__all__'
-        
+
+
 class SupporterSerializer(serializers.ModelSerializer):
     class Meta:
         model = Supporter
-        fields = '__all__' 
-        
+        fields = '__all__'
+
+
 class BoardSerializer(serializers.ModelSerializer):
     class Meta:
         model = Board
-        fields = '__all__'   
-        
+        fields = '__all__'
+
+
 class ConversationSerializer(serializers.ModelSerializer):
     class Meta:
         model = Conversation
-        fields = '__all__'                                           
-        
+        fields = '__all__'
+
+
 class MessageSerializer(serializers.ModelSerializer):
     class Meta:
         model = Message
-        fields = '__all__'        
+        fields = '__all__'
