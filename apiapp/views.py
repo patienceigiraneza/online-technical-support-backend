44e49fe0992340abb01831ba628c29290c9e4fce
patienceigiraneza <patienceigiraneza2@gmail.com>
2023-03-30 09:55:13 +0200

    add registration

--- apiapp/views.py
+++ apiapp/views.py
@@ -1,6 +1,19 @@
 from django.shortcuts import render
+from rest_framework import status
+from rest_framework.decorators import api_view
+from .serializers import *
+from rest_framework.response import Response
 
 # Create your views here.
+
+
+def fn_rest_register(request):
+    def_data = UserSerializer(data = request.data)
+    if def_data.is_valid():
+        def_data.save()
+        return Response(def_data.data, status=status.HTTP_201_CREATED)
+    return Response(def_data.errors, status=status.HTTP_400_BAD_REQUEST)
+
 
 def fn_home(request):
     pass
