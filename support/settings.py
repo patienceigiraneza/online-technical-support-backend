0c57458a084743ef4aec6a650b137f7704b8a14c
patienceigiraneza <patienceigiraneza2@gmail.com>
2023-05-03 16:18:25 +0200

    Offline db

--- support/settings.py
+++ support/settings.py
@@ -96,22 +96,22 @@ WSGI_APPLICATION = 'support.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'railway',
-        'USER': 'postgres',
-        'PASSWORD': 'CuqoOMP5uVycPUUbwV9d',
-        'HOST': 'containers-us-west-176.railway.app',
-        'PORT': '6773',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': 'railway',
+#         'USER': 'postgres',
+#         'PASSWORD': 'CuqoOMP5uVycPUUbwV9d',
+#         'HOST': 'containers-us-west-176.railway.app',
+#         'PORT': '6773',
+#     }
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
