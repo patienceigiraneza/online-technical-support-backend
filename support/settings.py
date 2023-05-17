b44a4291e31b910a6ab4998d3a4189a97024bdae
patienceigiraneza <patienceigiraneza2@gmail.com>
2023-05-17 21:01:28 +0200

    access token lifetime error

--- support/settings.py
+++ support/settings.py
@@ -92,8 +92,8 @@ REST_FRAMEWORK = {
 }
 
 SIMPLE_JWT = {
-    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=2),
-    "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
+    "ACCESS_TOKEN_LIFETIME": timedelta(days=1),
+    "REFRESH_TOKEN_LIFETIME": timedelta(days=2),
 }
 
 WSGI_APPLICATION = 'support.wsgi.application'
